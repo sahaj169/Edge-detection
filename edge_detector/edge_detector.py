6ec82a42394ecd1cd0d7d3b5d0284a42bbbbc365
sahaj169 <67413998+sahaj169@users.noreply.github.com>
2021-03-02 12:00:21 +0530

    Add files via upload

---
+++ edge_detector/edge_detector.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('image.jpeg',0)
+
+height , width = image.shape
+
+# extract sobel Edges - It emphasis Horizontal and vertical Edges 
+sobel_x = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize =5)
+sobel_y = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5)
+
+cv2.imshow('Original', image)
+cv2.waitKey(0)
+cv2.imshow('Sobel X', sobel_x) #horizontal 
+cv2.waitKey(0)
+cv2.imshow('Sobel Y', sobel_y)  # Vertical
+cv2.waitKey(0)
+
+sobel_OR = cv2.bitwise_or(sobel_x,sobel_y)
+cv2.imshow('sobel_OR', sobel_OR)  # both combined
+cv2.waitKey(0)
+
+# now get all the orientation - length and breadth 
+laplacian = cv2.Laplacian(image,cv2.CV_64F)
+cv2.imshow('Laplacian', laplacian)  # laplacian
+cv2.waitKey(0)
+
+#Applies thresholds i.e if a pixel is within upper and lower
+#thresholds, it is is considered as an edge
+
+canny = cv2.Canny(image, 50, 120)
+cv2.imshow('Canny', canny)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
